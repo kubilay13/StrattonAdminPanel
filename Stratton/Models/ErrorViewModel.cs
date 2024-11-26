5b5736767b964657cf0e16207d0c90e97e906283
kubilay13 <145432835+kubilay13@users.noreply.github.com>
2024-11-27 01:47:23 +0300

    Proje dosyası ekle.

---
+++ Stratton/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Stratton.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
