193fd41cd91e828249223cc9a7aa0db9a2b254ad
kubilay13 <145432835+kubilay13@users.noreply.github.com>
2024-12-01 20:51:32 +0300

    Listing of admin, moderator and premium users in the admin panel has been completed.

--- Stratton/Controllers/AdminController.cs
+++ Stratton/Controllers/AdminController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Stratton.Database;
 
 namespace Stratton.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly ApplicationDbContext _Context;
+        
+        public AdminController(ApplicationDbContext applicationDbContext)
+        {
+            _Context = applicationDbContext;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult AdminUsers()
+        {
+            var adminuser = _Context.AdminUsers.ToList();
+            return View(adminuser);
+        }
+
+        public IActionResult ModeratorUser()
+        {
+            var moderatoruser = _Context.moderatorUsers.ToList();
+            return View(moderatoruser);
+        }
+
+        public IActionResult PremiumUser()
+        {
+            var premiumuser = _Context.premiumUsers.ToList();
+            return View(premiumuser);
         }
     }
 }
